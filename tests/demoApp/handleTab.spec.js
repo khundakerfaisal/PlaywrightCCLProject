acff9ce0ab944b67f81fbac79f2d59ddb0507093
khundakerfaisal <khundakermis@gmail.com>
2025-09-01 19:30:13 +0600

    added tab handle in playwright

--- tests/demoApp/handleTab.spec.js
+++ tests/demoApp/handleTab.spec.js
@@ -1,17 +1,14 @@
-import {test,expect,chromium} from'@playwright/test'
+import { test, expect, chromium } from '@playwright/test'
 
-test('handle tab',async()=>{
-    const browser=await chromium.launch();
-    const contex=await browser.newContext()
-
-    const page1=await contex.newPage()
-
-    await page1.goto("https://demoqa.com/browser-windows")
-
-    const [newTab]=await Promise.all([contex.waitForEvent('page'),page1.click('#tabButton')])
-    const assertPage =await expect(newTab.getByText("This is a sample page")).toBeVisible()
+test('handle tab', async () => {
+    const browser = await chromium.launch(); //First config the chormium browser
+    const contex = await browser.newContext()//Create a contex browser
+    const page = await contex.newPage()//Added pages in new context
+    await page.goto("https://demoqa.com/browser-windows")
+    const [newTab]=await Promise.all([contex.waitForEvent('page'),page.click('#tabButton')]) //Handle multiple browser tab with click event
+    const assertPage = await expect(newTab.getByText("This is a sample page")).toBeVisible() //assert new tab heading
     console.log(assertPage)
-    await newTab.close();
+    await newTab.close();//Close the new tab
 })
 
 //npx playwright test tests/demoApp handleTab.spec.js
